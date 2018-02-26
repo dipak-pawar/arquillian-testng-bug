8b9c40651c6b829120cc5894b9e360f3aad94d6c
Dipak Pawar <dpawar@redhat.com>
2018-02-26 14:56:23 +0530

    adds testng reproducer for #cbeust/testng/issues/1677

--- src/test/java/com/example/SomeIT.java
+++ src/test/java/com/example/SomeIT.java
@@ -1,30 +1,41 @@
+/*
+/*/
+//*
+
 package com.example;
 
 import java.io.IOException;
-
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.testng.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-import org.jboss.shrinkwrap.resolver.api.maven.PomEquippedResolveStage;
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 public class SomeIT extends Arquillian {
 
     @Deployment
     public static Archive<?> createDeploymentPackage() throws IOException {
-        PomEquippedResolveStage pom = Maven.resolver().loadPomFromFile( "pom.xml" );
-
         return ShrinkWrap.create(WebArchive.class, "test.war")
-                .addClass(SomeIT.class)
-                .addAsLibraries( pom.resolve( "org.testng:testng" ).withTransitivity().asFile() );
+           .addClass(SomeIT.class);
     }
 
     @Test
     public void shouldAlwaysFail() {
-        Assert.assertFalse( true );
+        Assert.assertFalse(true);
+    }
+
+    @Test(expectedExceptions = IllegalStateException.class)
+    public void testHomePageHasAHeader() {
+        throw new IllegalStateException("");
+    }
+
+    @Test
+    public void foo() {
+        System.out.println("Hello:::::::::::::::::::::");
+        throw new SkipException("Skipping this exception");
     }
 }
+
